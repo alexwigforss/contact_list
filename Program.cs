c21ea442830d014adfaebf277f6c73a1ed394705
Alex Wigforss <alex.wigforss@gmail.com>
2023-03-20 10:39:36 +0100

    added tasks as comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿namespace dtp6_contacts
+﻿// TASK: 6. bryt ut static-metoder när du ser kodrepetitioner,
+// för varje ny static-metod a. kompilera/kör/testa, b. stage/commit/push!
+
+// TASK: 7. ta bort onödiga spårutskrifter,
+// gör en enda a. kompilering/körning/test, b. stage/commit/push!
+
+// TASK: 8. kommentera för att begripa koden, kommentera gärna alla metoder (static or dynamic) som
+// du känner för,
+// gör en enda a. kompilering/körning/test, b. stage/commit/push!
+
+// TASK: 9. om du hittar variabler som du tycker är svårbegripliga, döp om dem till något begripligt,
+// för varje variabeländring gör kompilera, gör en enda slutlig a. kompilering/körning/test,
+// och sedan b. stage/commit/push!
+
+// TASK: 10. plocka ut funktionaliteterna i if-else-kedjan genom att göra dem till metoder, som det passar
+// ändamålet,
+// för varje utbrytning a. kompilera/kör/testa, b. stage/commit/push!
+
+// TASK: 11. bygg smarta konstruktorer, setters och getters (kanske även properties) som det passar
+// ändamålet, men i synnerhet setters och getters för attributen phone och address,
+// gör a. kompileringar/körningar/tester, b. stage/commit/push som det passar!
+
+namespace dtp6_contacts
 {
     class MainClass
     {
